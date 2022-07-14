b75d40eb03a34ad46f8b0c9f0ca10fb9bf73854b
JaturongSarak <93725807+JaturongSarak@users.noreply.github.com>
2022-07-14 14:16:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,35 @@ import tkinter
 app = tkinter.Tk()
 app.title('Price Calculator')
 
-price_front = 26 # (฿)
-gp = 30 # (%)
-vat = 7 # (%)
-gp_vat = gp+(gp*(vat/100)) # (%)
-
 def start():
-    return math.ceil((price_front/(100-gp_vat))*100)
+    price_front = int(price_entry.get()) # (฿)
+    gp = int(gp_entry.get()) # (%)
+    vat = int(vat_entry.get()) # (%)
+    gp_vat = gp+(gp*(vat/100)) # (%)
+    result_recommend_label.configure(text=math.ceil((price_front/(100-gp_vat))*100))
 
-print(start())
+gp_label = tkinter.Label(app, text='GP (%) : ')
+vat_label = tkinter.Label(app, text='VAT (%) : ')
+price_label = tkinter.Label(app, text='Price (฿) : ')
+recommend_label = tkinter.Label(app, text='Recommend (฿) : ')
+result_recommend_label = tkinter.Label(app, text='')
+
+gp_entry = tkinter.Entry(app)
+vat_entry = tkinter.Entry(app)
+price_entry = tkinter.Entry(app)
+
+calculator_button = tkinter.Button(app, text='Calculate', command=start)
+
+gp_label.grid(column=0, row=0, padx=5, pady=5)
+vat_label.grid(column=0, row=1, padx=5, pady=5)
+price_label.grid(column=0, row=2, padx=5, pady=5)
+recommend_label.grid(column=0, row=3, padx=5, pady=5)
+result_recommend_label.grid(column=1, row=3, padx=5, pady=5)
+
+gp_entry.grid(column=1, row=0, padx=5)
+vat_entry.grid(column=1, row=1, padx=5)
+price_entry.grid(column=1, row=2, padx=5)
+
+calculator_button.grid(column=2, row=1, padx=5)
+
+app.mainloop()
